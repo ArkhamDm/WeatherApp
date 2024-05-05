6294c2ea8a743857240cc6c72879fc70ce50b541
Dmitry <ada16091609@mail.ru>
2024-05-06 02:17:06 +0300

    add getting location of phone

--- app/src/main/java/dev/arkhamd/wheatherapp/ui/map/MapFragment.kt
+++ app/src/main/java/dev/arkhamd/wheatherapp/ui/map/MapFragment.kt
@@ -1,10 +1,17 @@
 package dev.arkhamd.wheatherapp.ui.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
@@ -15,9 +22,12 @@ import com.yandex.mapkit.map.Map
 import dev.arkhamd.wheatherapp.databinding.FragmentMapBinding
 import dev.arkhamd.wheatherapp.ui.map.dialog.MapOnSelectSpotDialog
 
+
 class MapFragment : Fragment() {
     private lateinit var binding: FragmentMapBinding
     private lateinit var map: Map
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1001
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val geoObjectTapListener = GeoObjectTapListener {
         val point = it.geoObject.geometry.firstOrNull()?.point ?: return@GeoObjectTapListener true
@@ -44,6 +54,59 @@ class MapFragment : Fragment() {
     ): View {
         binding = FragmentMapBinding.inflate(inflater)
         MapKitFactory.initialize(this.context)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this.requireContext())
+
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location ->
+                            // Получение последнего известного местоположения
+                            if (location != null) {
+                                START_POSITION = CameraPosition(
+                                    Point(location.latitude, location.longitude),
+                                    8f,
+                                    0f,
+                                    30f
+                                )
+                            }
+                        }
+                        .addOnFailureListener { _ ->
+                            Toast.makeText(this.context, "Ошибка местоположения", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+
+
+
+        when {
+            ContextCompat.checkSelfPermission(
+                this.requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location ->
+                        if (location != null) {
+                            START_POSITION = CameraPosition(
+                                Point(location.latitude, location.longitude),
+                                8f,
+                                0f,
+                                30f
+                            )
+                        }
+                    }
+                    .addOnFailureListener { _ ->
+                        Toast.makeText(this.context, "Ошибка местоположения", Toast.LENGTH_SHORT).show()
+                    }
+            }
+            else -> {
+                // ask for permission
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+            }
+        }
 
         map = binding.yandexMap.mapWindow.map
         map.move(
@@ -66,34 +129,11 @@ class MapFragment : Fragment() {
         super.onStop()
     }
 
-    /*private fun isLocationPermissionGranted(): Boolean {
-        return if (ActivityCompat.checkSelfPermission(
-                this.requireContext(),
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this.requireContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this.requireActivity().parent,
-                arrayOf(
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                requestCode
-            )
-            false
-        } else {
-            true
-        }
-    }*/
-
     companion object {
         private val START_ANIMATION = Animation(Animation.Type.LINEAR, 1f)
         private val SMOOTH_ANIMATION = Animation(Animation.Type.SMOOTH, 0.4f)
 
-        private val START_POSITION = CameraPosition(
+        private var START_POSITION = CameraPosition(
             Point(59.938732, 30.316229),
             8f,
             0f,
